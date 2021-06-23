4c4bb854c8f78e1f1d6897fbd5a3211810bcf462
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-23 11:03:23 +0200

    `xtask`: install `flip-link` + run `cargo test`

--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> anyhow::Result<()> {
+    println!("\n⏳ install latest flip-link");
+    cargo::install_flip_link()?;
+
+    println!("\n🧪 cargo test");
+    cargo::test()?;
+
+    // ---
+    Ok(())
+}
+
+mod cargo {
+    use std::process::Command;
+
+    /// Install local revision of `flip-link`.
+    pub fn install_flip_link() -> anyhow::Result<()> {
+        let status = Command::new("cargo")
+            .args(&["install", "--path", "."])
+            .status()?;
+        match status.success() {
+            false => Err(anyhow::anyhow!("installing flip-link from path")),
+            true => Ok(()),
+        }
+    }
+
+    pub fn test() -> anyhow::Result<()> {
+        let status = Command::new("cargo").arg("test").status()?;
+        match status.success() {
+            false => Err(anyhow::anyhow!("running `cargo test`")),
+            true => Ok(()),
+        }
+    }
 }
