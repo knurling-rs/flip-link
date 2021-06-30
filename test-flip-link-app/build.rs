5ccfa26e89dc72be5cc35b2faab7055dad001651
Johann Hemmann <johann.hemmann@code.berlin>
2021-06-30 19:44:10 +0200

    Rename `*.x` to `.*.x`

--- test-flip-link-app/build.rs
+++ test-flip-link-app/build.rs
@@ -6,12 +6,12 @@ fn main() -> anyhow::Result<()> {
 
         let out = PathBuf::from(env::var_os("OUT_DIR").expect("`OUT_DIR` is not set"));
 
-        File::create(out.join("memory.x"))?.write_all(include_bytes!("memory.x"))?;
-        File::create(out.join("empty.x"))?.write_all(include_bytes!("empty.x"))?;
+        File::create(out.join("memory.x"))?.write_all(include_bytes!(".memory.x"))?;
+        File::create(out.join("empty.x"))?.write_all(include_bytes!(".empty.x"))?;
 
         println!("cargo:rustc-link-search={}", out.display());
-        println!("cargo:rerun-if-changed=memory.x");
-        println!("cargo:rerun-if-changed=empty.x");
+        println!("cargo:rerun-if-changed=.memory.x");
+        println!("cargo:rerun-if-changed=.empty.x");
         println!("cargo:rerun-if-changed=build.rs");
     }
 
